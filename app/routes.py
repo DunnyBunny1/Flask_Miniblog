04519c064f3a6d1544bc50b2e851121878b682af
DunnyBunny1 <murray.don@northeastern.edu>
2024-05-08 12:23:20 -0400

    Separated out presentation logic for base page into templates directory, implemented Jinja inheritance to display base page content

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,21 @@
 from app import app
+from flask import render_template
 
-@app.route('/')
-@app.route('/index')
-# This index view function will be triggered whenever either endpoint
-# specified above is requested when a browser requests this URL. The return
-# value of the function will be returned to the browser 
+
+@app.route("/")
+@app.route("/index")
+# This view function will be triggered whenever either endpoint specified
+# above is requested when a browser requests this URL. The return
+# value of the function will be returned to the browser
 def index():
-    return 'Hello, World!'
+    # Create a mock user
+    user = {"username": "Miguel"}
+    posts = get_posts()
+    return render_template("index.html", title="Home", user=user, posts= posts)
+
+def get_posts(): 
+    return [ 
+        {'author' : {'username' : 'John'},'body' : 'Beautiful Day in Boston!'},
+        {'author' : {'username' : 'Susan'}, 'body' : 'That movie was tuff'},
+        {'author' : {'username' : 'Bob'}, 'body' : 'The Celtics are the greatest team in the world'},
+    ]
